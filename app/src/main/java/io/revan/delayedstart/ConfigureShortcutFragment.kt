2ba6ccc487bed1545794b7f5d15462fc0a9abe9f
Revan Sopher <rsopher@gmail.com>
2020-03-31 23:45:20 -0700

    Reuse app icon from wrapped app.

--- app/src/main/java/io/revan/delayedstart/ConfigureShortcutFragment.kt
+++ app/src/main/java/io/revan/delayedstart/ConfigureShortcutFragment.kt
@@ -3,10 +3,14 @@ package io.revan.delayedstart
 import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.pm.ShortcutInfoCompat
 import android.support.v4.content.pm.ShortcutManagerCompat
+import android.support.v4.graphics.drawable.IconCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -61,16 +65,18 @@ class ConfigureShortcutFragment : Fragment() {
             }
     }
 
-    fun createShortcut(pkg: String, cls: String, name: String)  {
+    private fun createShortcut(pkg: String, cls: String, name: String)  {
         val intent = Intent(Intent.ACTION_VIEW).apply {
             putExtra("pkg", pkg)
             putExtra("cls", cls)
             component = ComponentName("io.revan.delayedstart", "io.revan.delayedstart.DelayLaunchActivity")
         }
 
-        // TODO set app icon
         val pinShortcutInfo = ShortcutInfoCompat.Builder(context!!, UUID.randomUUID().toString())
-            .setIntent(intent).setShortLabel(name).build()
+            .setIcon(makeIconCompatFromDrawable(context!!.packageManager.getApplicationIcon(pkg)))
+            .setIntent(intent)
+            .setShortLabel(name)
+            .build()
 
         // TODO handle this failing (lower API versions?)
         val pinnedShortcutCallbackIntent = ShortcutManagerCompat.createShortcutResultIntent(context!!, pinShortcutInfo)
@@ -83,5 +89,18 @@ class ConfigureShortcutFragment : Fragment() {
         ShortcutManagerCompat.requestPinShortcut(context!!, pinShortcutInfo,
             successCallback.intentSender)
 
+    }
+
+    private fun makeIconCompatFromDrawable(drawable: Drawable): IconCompat {
+        val bitmap = Bitmap.createBitmap(
+            if (drawable.intrinsicWidth == -1) 1 else drawable.intrinsicWidth,
+            if (drawable.intrinsicHeight == -1) 1 else drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        Canvas(bitmap).let {
+            drawable.setBounds(0, 0, it.width, it.height)
+            drawable.draw(it)
+        }
+        return IconCompat.createWithBitmap(bitmap)
     }
 }
