10a68c143557d5ce65a43e3003a0244f7ac101f6
Revan Sopher <rsopher@gmail.com>
2020-04-12 21:56:39 -0700

    Delete unused settings menu.

--- app/src/main/java/io/revan/delay/MainActivity.kt
+++ app/src/main/java/io/revan/delay/MainActivity.kt
@@ -2,8 +2,6 @@ package io.revan.delay
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.findNavController
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,22 +12,6 @@ class MainActivity : AppCompatActivity(), ChooseAppFragment.OnListFragmentIntera
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onListFragmentInteraction(item: InstalledApp?) {
